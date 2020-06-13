0a8226d164f5f6e21da14ce968cfbf2218896a8d
Quentin GRANCHER <quentingrancher@ANTI-SNITCH-SNITCH-CLUB.local>
2020-06-13 17:49:39 +0200

    add redirect after post in db

--- public/index.php
+++ public/index.php
@@ -3,34 +3,9 @@ session_start();
 require "../controller/controller.php";
 
 if(isset($_SESSION['check'])){
-        
-  echo "connected";
+
 }
 
-// class PDOConnexion
-// {
-//     private $db_host = "127.0.0.1:3306";
-//     private $db_user = 'root';
-//     private $db_pass = 'root';
-//     private $db_name = 'voyager';
-
-//     public function connect(){
-//         $mysql_connect_str = "mysql:host=$this->db_host;dbname=$this->db_name";
-//         $dbConnection = new PDO($mysql_connect_str, $this->db_user, $this->db_pass,[
-//             PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
-//         ]);
-
-//         return $dbConnection;
-//     }
-// }
-
-
-// $pdo = new PDOConnexion();
-// $db = $pdo->connect();
-// $stmt = $db->query('SELECT * FROM polaroids');
-// $res = $stmt->fetchAll();
-
-// var_dump($res);
 include '../includes/post-db.php';
   if(isset($_GET['action'])) {
    
