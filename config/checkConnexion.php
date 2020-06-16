7a5fa0948ef05f4dd37bcceb1b6cd7df7dc026aa
bastien17-dev <bastien.paquier@gmail.com>
2020-06-16 16:50:54 +0200

    set pdo error mstry 5

--- config/checkConnexion.php
+++ config/checkConnexion.php
@@ -4,13 +4,23 @@
 class checkConnexion
 {
     private string $password;
+    private string $db_host = "custom-pcvp.mysql.eu2.frbit.com";
+    private string $db_user = 'custom-pcvp';
+    private string $db_pass = 'ClblgnkUMoXyy03hhy_whnlM';
+    private string $db_name = 'custom-pcvp';
 
     public function __construct(string $password){
         $this->password = $password;
     }
 
     public function checkPassword():bool {
-        if($this->password === 'password')
+        $mysql_connect_str = "mysql:host=$this->db_host;dbname=$this->db_name";
+        $pdo = new PDO($mysql_connect_str, $this->db_user, $this->db_pass, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+        $pdo->prepare('SELECT password FROM users where id_user = 1');
+        $password = $pdo->fetch();
+        if(password_verify($this->password, $password))
         {
             return true;
         }
